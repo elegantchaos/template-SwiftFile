5530b5310411458b552520c048b1ba6cd8524d3d
Sam Deane <sam@elegantchaos.com>
2020-06-12 17:06:21 +0100

    first commit

---
+++ xXxnamexXx.swift
@@ -0,0 +1,6 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by xXxuserxXx on xXxdatexXx.
+//  All code (c) xXxyearxXx - present day, xXxownerxXx.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import Foundation
